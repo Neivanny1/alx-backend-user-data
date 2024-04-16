df38a62e9e98de8bea30da5e5003ec8af69391ec
Neivanny1 <nevileneivanny@gmail.com>
2024-04-16 14:21:44 +0000

    fixed import issue

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -9,6 +9,7 @@ from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.views import app_views
 from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 
 
 app = Flask(__name__)
