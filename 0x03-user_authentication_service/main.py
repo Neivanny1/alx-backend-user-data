04f1f56773022278a1b2921dcb9a7e8b46eef20f
Neivanny1 <nevileneivanny@gmail.com>
2024-04-25 21:33:03 +0000

    4. Hash password

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -2,14 +2,6 @@
 """
 Main file
 """
+from auth import _hash_password
 
-from db import DB
-from user import User
-
-my_db = DB()
-
-user_1 = my_db.add_user("test@test.com", "SuperHashedPwd")
-print(user_1.id)
-
-user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
-print(user_2.id)
+print(_hash_password("Hello Holberton"))
